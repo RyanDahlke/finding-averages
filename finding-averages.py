1f897337db65916abd3c8000117a8a5477ac7b7c
Ryan <ryandahlke@gmail.com>
2014-12-07 05:13:20 +0000

    first commit

---
+++ finding-averages.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+"""A python script that takes inputted integers and returns the mean, median and mode."""
+
+__author__      = "Ryan Dahlke"
+__copyright__   = "GNU GPL v2.0"
+
+#   )\  )\  )\  )\  )\  )\  )\  )\  )\  )\  )\  )\  )\  )\  )\  )\  )\  )\
+#  (  \(  \(  \(  \(  \(  \(  \(  \(  \(  \(  \(  \(  \(  \(  \(  \(  \(  \
+
