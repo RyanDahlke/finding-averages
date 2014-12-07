51b0eb6e41d4924c79b19ae4be7b1196cc9993a6
Ryan <ryandahlke@gmail.com>
2014-12-07 05:14:02 +0000

    header prepared

--- finding-averages.py
+++ finding-averages.py
@@ -2,7 +2,7 @@
 
 """A python script that takes inputted integers and returns the mean, median and mode."""
 
-__author__      = "Ryan Dahlke"
+__author__      = "@RyanDahlke"
 __copyright__   = "GNU GPL v2.0"
 
 #   )\  )\  )\  )\  )\  )\  )\  )\  )\  )\  )\  )\  )\  )\  )\  )\  )\  )\
